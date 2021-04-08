65256b05a9ae447aee4f8f3b4016073a8433fdf6
npee <jmc4664@gmail.com>
2021-04-08 21:48:17 +0900

    Rename module

--- chapter01/src/main/java/chapter01/Main.java
+++ chapter01/src/main/java/chapter01/Main.java
@@ -6,10 +6,11 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 public class Main {
-    private static Logger log = LoggerFactory.getLogger(Main.class);
+    private static Logger log = LoggerFactory.getLogger(chapter01.Main.class);
 
     public static void main(String[] args) throws IOException {
         BlockingIOExample chapter01_1 = new BlockingIOExample();
         chapter01_1.serve(8888);
     }
 }
+
