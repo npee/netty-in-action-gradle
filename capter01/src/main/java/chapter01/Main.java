fbaf76775f031b491621032fdd9545cab61c7554
npee <jmc4664@gmail.com>
2021-04-08 21:40:43 +0900

    BlockingIOExample.java

---
+++ capter01/src/main/java/chapter01/Main.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+        BlockingIOExample chapter01_1 = new BlockingIOExample();
+        chapter01_1.serve(8888);
+    }
+}
