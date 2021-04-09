4353eb4b1a0b2f26473bd34ef1d06cfeb0bf4b94
npee <jmc4664@gmail.com>
2021-04-10 00:15:01 +0900

    Add Comment to BlockingIOExample Update README.md

--- chapter01/README.md
+++ chapter01/README.md
@@ -1,4 +1,9 @@
 # Chapter 01
-## BlockingIOExample
+## BlockingIOExample.java
+### serve() / processRequest()
+1. 서버 소켓을 열어 listen 상태로 둔다.
+2. 클라이언트 소켓을 listen 상태의 서버 소켓에 연결한다.
+3. I/O 스트림을 구성한다.
+4. "Done" 이라는 문자열을 받을 때까지 스트림을 문장 단위로 읽어들이며, 읽기 성공 시 "Processed"를 반환한다..
 ## ConnectExample
 ## ConnectHandler
--- chapter01/src/main/java/chapter01/BlockingIOExample.java
+++ chapter01/src/main/java/chapter01/BlockingIOExample.java
@@ -13,11 +13,16 @@ import java.net.Socket;
 public class BlockingIOExample {
     private static Logger log = LoggerFactory.getLogger(BlockingIOExample.class);
 
+    /**
+     * Chapter 01 - 1.1 Blocking I/O Example
+     */
     public void serve(int portNumber) throws IOException {
         ServerSocket serverSocket = new ServerSocket(portNumber);
         Socket clientSocket = serverSocket.accept();
+
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
         String request, response;
         while ((request = in.readLine()) != null) {
             if ("Done".equals(request)) {
